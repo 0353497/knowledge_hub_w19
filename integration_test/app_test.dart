c48e3a61f2a599ca1e2804b1a04ae5999cc582f8
Niek Geerligs <geerligsniek@gmail.com>
2026-05-09 10:06:45 +0200

    should work now

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -12,7 +12,7 @@ void main() {
 
     Future<void> nextStep(String description) async {
       print("Step No: $currentStep,$description");
-      await Future.delayed(Duration(seconds: 10));
+      await Future.delayed(Duration(seconds: 1));
       await tester.pumpAndSettle();
       currentStep++;
     }
@@ -81,5 +81,148 @@ void main() {
 
     await Future.delayed(Duration(seconds: 50));
     await tester.tap(find.text("Next"));
+    await tester.pumpAndSettle();
+
+    await nextStep(
+      "Complete the quiz on the Quiz Page (for the second knowledge item).",
+    );
+
+    await tester.tap(find.text("Next"));
+    await tester.pumpAndSettle();
+    Future.delayed(Duration(seconds: 2));
+
+    await selectQuestionAndNext("D");
+    await selectQuestionAndNext("D");
+    await selectQuestionAndNext("B");
+
+    await tester.tap(find.byKey(ValueKey("C")));
+    await tester.pumpAndSettle();
+    await tester.tap(find.text("Submit"));
+    await tester.pumpAndSettle();
+
+    expect(find.text("75"), findsOneWidget);
+    await nextStep(
+      "Review the score and answer summary on the Results Page for second knowledge item.",
+    );
+
+    await tester.tap(find.text("Exit"));
+    await tester.pumpAndSettle();
+
+    await nextStep("Select the third knowledge item from the Home Page.");
+    await tester.tap(find.text("Public Art"));
+    await tester.pumpAndSettle();
+
+    await nextStep("Play the third educational video on the Content Page.");
+    await tester.tap(find.text("Play"));
+    await tester.pumpAndSettle();
+
+    await Future.delayed(Duration(seconds: 50));
+    await tester.tap(find.text("Next"));
+    await tester.pumpAndSettle();
+
+    await nextStep(
+      "Complete the quiz on the Quiz Page (for the third knowledge item).",
+    );
+
+    await tester.tap(find.text("Next"));
+    await tester.pumpAndSettle();
+    Future.delayed(Duration(seconds: 2));
+
+    await selectQuestionAndNext("B");
+    await selectQuestionAndNext("C");
+    await selectQuestionAndNext("D");
+
+    await tester.tap(find.byKey(ValueKey("A")));
+    await tester.pumpAndSettle();
+    await tester.tap(find.text("Submit"));
+    await tester.pumpAndSettle();
+
+    expect(find.text("0"), findsOneWidget);
+    await nextStep(
+      "Review the score and answer summary on the Results Page for third knowledge item.",
+    );
+
+    await tester.tap(find.text("Exit"));
+    await tester.pumpAndSettle();
+
+    await nextStep("Select the fourth knowledge item from the Home Page.");
+    await tester.tap(find.text("Facilities"));
+    await tester.pumpAndSettle();
+
+    await nextStep("Play the fourth educational video on the Content Page.");
+    await tester.tap(find.text("Play"));
+    await tester.pumpAndSettle();
+
+    await Future.delayed(Duration(seconds: 54));
+    await tester.tap(find.text("Replay"));
+    await tester.pumpAndSettle();
+    await Future.delayed(Duration(milliseconds: 2500));
+
+    await tester.tap(find.text("Next"));
+    await tester.pumpAndSettle();
+
+    await nextStep(
+      "Complete the quiz on the Quiz Page (for the fourth knowledge item).",
+    );
+
+    await tester.tap(find.text("Next"));
+    await tester.pumpAndSettle();
+    Future.delayed(Duration(seconds: 2));
+
+    await selectQuestionAndNext("A");
+    await selectQuestionAndNext("C");
+    await selectQuestionAndNext("B");
+
+    await tester.tap(find.byKey(ValueKey("D")));
+    await tester.pumpAndSettle();
+    await tester.tap(find.text("Submit"));
+    await tester.pumpAndSettle();
+
+    expect(find.text("50"), findsOneWidget);
+    await nextStep(
+      "Review the score and answer summary on the Results Page for fourth knowledge item.",
+    );
+
+    await tester.tap(find.text("Exit"));
+    await tester.pumpAndSettle();
+
+    await nextStep("Select the fifth knowledge item from the Home Page.");
+    await tester.tap(find.text("Dining"));
+    await tester.pumpAndSettle();
+
+    await nextStep("Play the fifth educational video on the Content Page.");
+    await tester.tap(find.text("Play"));
+    await tester.pumpAndSettle();
+
+    await Future.delayed(Duration(seconds: 110));
+    await tester.tap(find.text("Next"));
+    await tester.pumpAndSettle();
+
+    await nextStep(
+      "Complete the quiz on the Quiz Page (for the fifth knowledge item).",
+    );
+
+    await tester.tap(find.text("Next"));
+    await tester.pumpAndSettle();
+    Future.delayed(Duration(seconds: 2));
+
+    await selectQuestionAndNext("C");
+    await selectQuestionAndNext("D");
+    await selectQuestionAndNext("A");
+
+    await tester.tap(find.byKey(ValueKey("B")));
+    await tester.pumpAndSettle();
+    await tester.tap(find.text("Submit"));
+    await tester.pumpAndSettle();
+
+    expect(find.text("25"), findsOneWidget);
+    await nextStep(
+      "Review the score and answer summary on the Results Page for fifth knowledge item.",
+    );
+
+    await tester.tap(find.text("Exit"));
+    await tester.pumpAndSettle();
+
+    await nextStep("Return to the Home Screen.");
   });
 }
